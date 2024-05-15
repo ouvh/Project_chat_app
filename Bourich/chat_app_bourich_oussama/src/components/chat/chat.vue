c3910e17db3e44136fef557fc593c125c01439c9
Mohamedaymanbourich <Mohamed.BOURICH@um6p.ma>
2024-05-15 23:14:46 +0100

    yes

---
+++ Bourich/chat_app_bourich_oussama/src/components/chat/chat.vue
@@ -0,0 +1,167 @@
+<template>
+  <div class="chat">
+
+
+
+    <div class="top">
+
+      <div class="user">
+        <img src="../../../public/assets/avatar.png" alt="">
+        <div classname="texts">
+
+          <span>Jan Doe</span>
+          <p>Lorem ipsum dolor, sit amet </p>
+
+
+        </div>
+      </div>
+      <div class="icons">
+        <img src="../../../public/assets/phone.png" alt="abcd">
+          <img src="../../../public/assets/video.png" alt="fgervb">
+          <img src="../../../public/assets/info.png" alt="abcdevger">
+      </div> 
+          
+    </div>
+    <div class="center">
+
+
+    </div>
+    <div class="bottom">
+          <div class="icons">
+
+          <img src="../../../public/assets/img.png" alt="abcd">
+          <img src="../../../public/assets/camera.png" alt="fgervb">
+          <img src="../../../public/assets/mic.png" alt="abcdevger">
+
+          </div>
+          <input v-model="text" type="text" placeholder="Type a message ...">
+          <div class="emoji">
+              <img @click="toggleEmojiPicker" src="../../../public/assets/emoji.png">
+
+          </div> 
+          <button class="sendButton">Send</button>
+
+
+    </div>
+  </div>
+</template>
+
+<script>
+import { EmojiButton } from '@joeattardi/emoji-button';
+
+export default {
+  data() {
+    return {
+      text : "",
+      picker:  new EmojiButton()
+    };
+  },
+  
+  
+  methods: {
+    toggleEmojiPicker(event) {
+      this.picker.togglePicker(event.target);
+      this.picker.on('emoji', emoji => {
+        console.log(emoji)
+      this.text += emoji.emoji;
+    });
+    }
+  }
+};
+</script>
+
+<style scoped>
+.chat{
+    flex: 2;
+    border-left:1px solid #dddddd35;
+    border-right : 1px solid #dddddd35;
+    height: 100%;
+}
+.chat .top{
+padding: 20px;
+display : flex;
+align-items : center;
+justify-content: space-between;
+border-bottom: 1px solid #dddddd35;
+
+    
+
+}
+.top .user{
+    display : flex;
+    align-items : center;
+    gap : 20px ;
+    
+    }
+.user img {
+      width : 40px;
+      height : 40px;
+      border-radius : 50%; 
+      object-fit : cover
+    } 
+.texts{
+  display : flex;
+  flex-direction: column ;
+  gap : 5px
+}
+span{
+  font-size : 18px;
+  font-weight : bold ; 
+}
+p{
+  font-size : 14px;
+  font-weight : 300;
+  color : #a5a5a5
+
+}
+.icons{
+  display : flex ; 
+  gap : 20px;
+  
+}
+
+img{
+  width: 20px;
+  height: 20px;
+  cursor : pointer
+  }
+
+.center{
+
+
+
+}
+.bottom{
+
+  padding:20px;
+  display : flex;
+  align-items:center;
+  justify-content : space-between;
+  gap : 20px;
+  border-top : 1px solid #dddddd35;
+}
+
+ .bottom  input{
+  flex :1;
+  background-color: rgba(17,25,40,0.5);
+  border : none;
+  outline : none;
+  color : white ;
+  border-radius: 10px;
+  font-size : 16px;
+  padding : 20px
+}
+
+.bottom .sendButton{
+
+  background-color : #5183fe;
+  color : white ;
+  padding : 10px 20px;
+  border : none;
+  border-radius : 5px;
+  cursor : pointer 
+
+
+}
+
+</style>
---
+++ Bourich/chat_app_bourich_oussama/src/components/list/list.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="list">
+        <userinfo/>
+        <chatlist/>
+    </div>
+
+</template>
+
+<script>
+import chatlist from './chatlist/chatlist.vue'
+import userinfo from "./userinfo/userinfo.vue"
+export default {
+    components:{userinfo,chatlist}
+
+}
+</script>
+
+<style scoped>
+.list{
+    flex: 1;
+}
+
+</style>
