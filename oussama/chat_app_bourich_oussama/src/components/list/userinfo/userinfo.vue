ca58ebbf85a438ebee14c951e01d8935456c76b3
ouvh <oussamalaaroussiFCB@gmail.com>
2024-05-15 21:00:58 +0100

    new

--- oussama/chat_app_bourich_oussama/src/components/list/userinfo/userinfo.vue
+++ oussama/chat_app_bourich_oussama/src/components/list/userinfo/userinfo.vue
@@ -1,8 +1,16 @@
 <template>
 <div class="userinfo">
-    <div class="user"></div>
+    <div class="user">
+        <img src="../../../../public/assets/avatar.png" alt="" srcset="">
+        <h2>John Doe</h2>
+    </div>
+
+
     <div class="icons">
-        <img src="" alt="" srcset="">
+        <img src="../../../../public/assets/more.png" alt="" srcset="">
+        <img src="../../../../public/assets/video.png" alt="" srcset="">
+        <img src="../../../../public/assets/edit.png" alt="" srcset="">
+
     </div>
 
 </div>
@@ -17,7 +25,38 @@ export default {
 
 <style scoped>
 .userinfo{
+    padding: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 
 }
 
+.user{
+    display: flex;
+    align-items: center;
+    gap: 20px;
+   
+
+}
+
+.user img{
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    object-fit: cover ;
+
+}
+
+
+.icons{
+    display: flex;
+    gap: 20px;
+}
+
+.icons img {
+    width: 20px;
+    height: 20px;
+    cursor: pointer;
+}
 </style>
